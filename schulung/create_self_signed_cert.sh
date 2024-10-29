0d9e40f40f82f543f5fd0cccc5546538d243e722
mg0050 <michael@gortat.com>
2024-10-29 20:52:15 +0000

    added missing lines

--- schulung/create_self_signed_cert.sh
+++ schulung/create_self_signed_cert.sh
@@ -9,3 +9,10 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -extensions SAN \
     -config <(cat /etc/ssl/openssl.cnf \
 <(printf "[SAN]\nsubjectAltName=DNS:${DOM}.com,DNS:*.${DOM}"))
+
+kubectl delete secret ${DOM}-tls --ignore-not-found -n argocd
+ 
+kubectl create secret tls ${DOM}-tls --key ${DOM}.key --cert ${DOM}.crt -n argocd
+ 
+kubectl delete -f argocd-ingress.yaml --ignore-not-found
+kubectl apply -f argocd-ingress.yaml
